d6b13a003d7c921bdc42e002c14121b5dd9f9237
night-sornram <6633236021@student.chula.ac.th>
2024-12-04 11:01:21 +0700

    Add Docker Compose setup for Kafka and Zookeeper, and implement Avro serialization/deserialization in main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import avro.schema
+import avro.io
+import io
+import hashlib, json
+
+def serialize(schema, obj):
+    bytes_writer = io.BytesIO()
+    encoder = avro.io.BinaryEncoder(bytes_writer)
+    writer = avro.io.DatumWriter(schema)
+    writer.write(obj, encoder)
+    return bytes_writer.getvalue()
+
+def deserialize(schema, raw_bytes):
+    bytes_reader = io.BytesIO(raw_bytes)
+    decoder = avro.io.BinaryDecoder(bytes_reader)
+    reader = avro.io.DatumReader(schema)
+    return reader.read(decoder)
+
+kafka_broker = 'localhost:9092'
+
+
