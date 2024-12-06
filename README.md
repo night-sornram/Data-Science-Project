a66b2d2d407f59a8f4060e366310fb47b12d53a9
night-sornram <6633236021@student.chula.ac.th>
2024-12-06 22:18:46 +0700

    Refactor Kafka data processing to loop through years 2018-2023, update README for execution steps, and enhance data cleaning by removing duplicates and handling missing values

--- README.md
+++ README.md
@@ -4,12 +4,25 @@ download Data in this link : https://drive.google.com/file/d/107WikNVtve-QY7I7-p
 
 Copy Project folder to this repo
 
-# Docker
-docker-compose up -d
-
-# Kafka
 python -m venv .venv
 
 .venv\Scripts\activate
 
 pip install -r requirements.txt
+
+Ternimal1 :
+    .venv\Scripts\activate
+
+    python consumer.py
+
+Terminal2 :
+    .venv\Scripts\activate
+
+    python main.py
+
+    ....wait until finished....
+
+    python clean_data.py
+
+# Docker
+docker-compose up -d
--- clean_data.py
+++ clean_data.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from keybert import KeyBERT
+model = KeyBERT('distilbert-base-nli-mean-tokens')
 
 column_names = ['title', 'abstract', 'classifications', 'date_publication', 'affiliation', 'references', 'keywords']
 
-df = pd.read_csv('./scopus_messages.csv', header=None, names=column_names, on_bad_lines='skip')
+df = pd.read_csv('./scopus_messages.csv', header=0, names=column_names, on_bad_lines='skip', usecols=['title', 'abstract', 'date_publication', 'keywords'])
 
 df.drop_duplicates(inplace=True)
+df.dropna(inplace=True)
 
 def convert_date(date_str):
     data = date_str.replace("{'year': '", "").replace("', 'month': '", ",").replace("', 'day': '", ",").replace("'}", "")
@@ -15,17 +18,23 @@ def convert_date(date_str):
         'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
         'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'
     }
-    if len(data) != 3:
-        return "date_plublication"
-    year = data[0]
-    month = month_map[data[1]]
-    day = f"{int(data[2]):02d}"
-    return f"{year}-{month}-{day}"
+    try:
+        year = data[0]
+        month = month_map[data[1]]
+        day = f"{int(data[2]):02d}"
+        return f"{year}-{month}-{day}"
+    except:
+        return None
+
+def find_keywords(df):
+    keywords = df["keywords"]
+    if len(keywords) == 0:
+        keywords = []
+        key = model.extract_keywords(df["abstract"])
+        for kw , sc in key:
+            keywords.append(kw)
+        df["keywords"] = keywords
 
 df["title"] = df["title"].apply(lambda x: x.replace("Title:", ""))
-
 df["date_publication"] = df["date_publication"].apply(convert_date)
-
-
-df.to_csv('scopus_messages_cleaned.csv', index=False, header=False)
-
+df.to_csv('scopus_messages_cleaned.csv', index=False, header=True)
